5ea21323a4f4fabee766301d04b2868d4bd19b18
conneralegre <cga0@protonmail.com>
2016-12-28 21:00:16 -0700

    Code cleanup

--- mainFunc.js
+++ mainFunc.js
@@ -1,19 +1,25 @@
 const convertData = (resources) => {
-	const ascendingResources = resources.sort((a, b) => a.order - b.order);
-	return ascendingResources.reduce((arr, curr, i) => {
-		let next = ascendingResources[(i + 1) % ascendingResources.length];
+    const ascendingResources = resources.sort((a, b) => a.order - b.order);
+    return ascendingResources.reduce((arr, curr, i) => {
+        let next = ascendingResources[(i + 1) % ascendingResources.length];
 
-		if (curr.type === 'Person' && next.type === 'Person' && ascendingResources.indexOf(next) !== 0) {
-			arr.push({
-				type: 'Person',
-				order: next.order,
-				people: [curr.name, next.name]
-			});
-			ascendingResources.splice(i, 1);
-		} 
-		else if (curr.type !== 'Person') { arr.push(curr); }
-		else if (curr.type === 'Person') { arr.push({ type: 'Person', order: curr.order, people: [curr.name] }); }
+        if (curr.type === 'Person' && next.type === 'Person' && ascendingResources.indexOf(next) !== 0) {
+            arr.push({
+                type: 'Person',
+                order: next.order,
+                people: [curr.name, next.name]
+            });
+            ascendingResources.splice(i, 1);
+        } else if (curr.type !== 'Person') {
+            arr.push(curr);
+        } else if (curr.type === 'Person') {
+            arr.push({
+                type: 'Person',
+                order: curr.order,
+                people: [curr.name]
+            });
+        }
 
-		return arr;
-	}, []);
+        return arr;
+    }, []);
 }
